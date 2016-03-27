709016a5a7c72f8fe2ebaf6935238dd58091b2ea
DSG <dsg@DSG-PC-L>
2016-03-27 12:52:01 +0600

    -

--- SAOD/LR2/main.c
+++ SAOD/LR2/main.c
@@ -1,7 +1,59 @@
+#include <ctype.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 
-int main( int argc, char *argv[] )
+#define IFILE "input.txt"
+
+const int YES = 1, NO = 0;
+int main()
 {
-	
+	uint32_t CountWord = 0, CountSpace = 0;
+	FILE* f;
+	// Попытка открыть файл
+	if((f = fopen(IFILE, "r")) == NULL)
+	{
+		// Провалилась
+		perror("fopen");
+		exit(1);
+	}
+	printf("\tЧтение файла: %s\n", IFILE);
+
+
+	register int inWord = NO; // Почему не short int?
+	char c; // Было у Кернигана и Ритчи int
+
+	int WLen = 0;
+	char out[100] = {'\0'};
+
+	while((c = getc(f)) != EOF)
+		if(isspace(c) || c == '\n')
+		{
+			inWord = NO;
+			++CountSpace;
+
+//Для вывода
+			if (WLen)
+			{
+				out[WLen] = '\0';
+				printf("%s %d\n", out, WLen);
+			}
+//
+
+			WLen = 0;
+		}
+		else
+		{
+			out[WLen] = c;
+			++WLen;
+
+			if(inWord == NO)
+			{
+
+				inWord = YES;
+				++CountWord;
+			}
+		}
+	printf("\tПрочитано слов: %d. Найдено пробелов/переносов: %d\n", CountWord, CountSpace-1);
 	return 0;
 }
