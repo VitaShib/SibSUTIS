792166616791c7f3465c70a74d44922330d068c5
DSG <NoEmail>
2016-04-17 19:45:12 +0600

    prog lr3

---
+++ Prog/II/LR3/input_output.h
@@ -0,0 +1,9 @@
+#ifndef INPUT_OUTPUT_H
+#define INPUT_OUTPUT_H
+
+#include <stdio.h>
+#include "processing.h"
+int input(...);
+
+
+#endif
---
+++ Prog/II/LR3/processing.h
@@ -0,0 +1,10 @@
+#ifndef PROCESSING_H
+#define PROCESSING_H
+
+#include <stdio.h>
+
+//int update(...);
+//int process(...);
+
+
+#endif
